9b12cf18c2faaf662fbbab38fa5371a19d63b946
Kadugs <cadu20.gomes@gmail.com>
2021-09-03 10:31:34 -0300

    implementado axios na tela de filmes e nas sessoes

--- src/components/Session/Session.js
+++ src/components/Session/Session.js
@@ -1,17 +1,33 @@
 import { useParams } from "react-router";
 import Days from "../Days/Days.js";
 import Footer from "../Footer/Footer";
+import URL from '../URL'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Session.css';
 import "../../App.css";
 export default function Session({ movies }) {
     const moviePage = Number(useParams().idMovie);
     const selectedMovie = movies.filter((movie) => {
-        if(movie.id === moviePage) {
+        if(Number(movie.id) === moviePage) {
             return true;
         }
         return false;
     })
-    console.log(selectedMovie[0])
+    const [sessions, setSessions] = useState([]);
+    useEffect(() => {
+        const request = axios.get(`${URL}/${selectedMovie[0].id}/showtimes`);
+        request.then(res => {
+            console.log(res.data);
+            setSessions(res.data);
+        }).catch (err => {
+            console.log(err);
+            alert('error: ' + err.message);
+        })
+    }, []);
+    if (sessions.length === 0) {
+        return (<p>Loading...</p>)
+    }
     return (
         <>
             <div className="title">
@@ -19,18 +35,18 @@ export default function Session({ movies }) {
             </div>
 
             <div className="option-of-days">
-            {selectedMovie[0].days.map((day, index) => (
-                <Days 
-                id={day.id}
-                weekday={day.weekday}
-                date={day.date}
-                showtimes={day.showtimes}
-                key={index}
-                />
+                {sessions.days.map((day, index) =>(
+                    <Days 
+                    id={day.id}
+                    weekday={day.weekday}
+                    date={day.date}
+                    showtimes={day.showtimes}
+                    key={index}
+                    />
                 ))}
                 <Footer 
-                    image={selectedMovie[0].posterURL}
-                    title={selectedMovie[0].title}
+                    image={sessions.posterURL}
+                    title={sessions.title}
                 />
             </div>
         </>
---
+++ src/components/URL.js
@@ -0,0 +1,2 @@
+const URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v3/cineflex/movies';
+export default URL;
