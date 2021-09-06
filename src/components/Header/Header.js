507140a4ef92dd35b32feb20b5620d3a4ebaf4ad
Kadugs <cadu20.gomes@gmail.com>
2021-09-06 15:06:37 -0300

    adicionado botao de voltar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,16 @@
+import { useHistory} from 'react-router-dom'
+import { useLocation } from 'react-router';
 import './Header.css';
-
+import imageBack from '../../assets/arrow-back-outline.svg';
 export default function Header() {
+    let history = useHistory();
+    const location = useLocation().pathname;
+    function returnPage() {
+         history.goBack();
+    }
     return (
         <header>
+            <img src={location !== '/' ? imageBack : ''} className="image-back" alt="" onClick={returnPage} />
             <h1>CINEFLEX</h1>
         </header>
     )
