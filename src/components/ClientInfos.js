f639cad0b495be39b140a2fa2d2ff73f0c1ef70f
Nivea <nivea@respondeai.com.br>
2021-09-06 19:38:50 -0300

    preparando para o review

--- src/components/ClientInfos.js
+++
@@ -1,8 +0,0 @@
-export default function ClientInfos({type, setUserInfo}) {
-    return (
-        <div className="input-client-infos">
-            <span>{type} do comprador:</span>
-            <input type="text" placeholder={`Digite seu ${type} ...`} onChange={(e) => setUserInfo(e.target.value)}></input>
-        </div>
-    );
-}
